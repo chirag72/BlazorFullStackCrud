7e02538a4530c7d490a27096cadd6e44ed55597b
Chirag <Chirag@TABLET-UP19T76V>
2022-03-02 16:59:58 -0500

    Add project files.

---
+++ BlazorFullStackCrud/Server/Data/DataContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorFullStackCrud.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Comic>().HasData(
+                new Comic { Id = 1, Name = "Marvel" },
+                new Comic { Id = 2, Name = "Dc" }
+            );
+            modelBuilder.Entity<SuperHero>().HasData(
+                new SuperHero
+                {
+                    Id = 1,
+                    FirstName = "Peter",
+                    LastName = "Patrker",
+                    HeroName = "SpiderMan",
+                    ComicId = 1
+                },
+                new SuperHero
+                {
+                    Id = 2,
+                    FirstName = "Bruce",
+                    LastName = "Mayne",
+                    HeroName = "BatMan",
+                    ComicId = 2
+                }
+           );
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+        public DbSet<Comic> Comics { get; set; }
+    }
+}
